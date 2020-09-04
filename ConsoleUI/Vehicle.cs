7a03459aeb1c2940ad1f4ff7fb5d1dfe655ab4ae
Sarah <sarah.b.avram@gmail.com>
2020-09-04 15:23:18 -0400

    new class

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public string Year = "2015";
+        public string Make = "Ford";
+        public string Model = "Focus";
+
+        public abstract void DriveAbstract();
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine("Drive Virtual");
+        }
+    }
+}
